eaf9af361ad2bc55e5d92a4a32a4edcbb830cc6b
Yves Desjardins <YvesDesjardins@gmail.com>
2019-02-07 12:25:16 -0800

    finalized map function and implemented console.log to test ballbacks

--- map.js
+++ map.js
@@ -1,16 +1,31 @@
+// Implement your own version of the built-in array map function.
+
+// Yours will take in two arguments. The first will be an array to map and the second 
+// will be a callback function. The function will return a new array based on the results 
+// of the callback function.
+
 var words = ["ground", "control", "to", "major", "tom"];
 
-map(words, function (word) {
+function map(list, callback) {
+  var newList = [];
+  list.forEach(set => {
+    newList.push(callback(set));
+  });
+
+  return newList;
+}
+
+console.log(map(words, function (word) {
   return word.length;
-});
+}));
 
-map(words, function (word) {
+console.log(map(words, function (word) {
   return word.toUpperCase();
-});
+}));
 
-map(words, function (word) {
+console.log(map(words, function (word) {
   return word.split('').reverse().join('');
-});
+}));
 
 // [6, 7, 2, 5, 3]
 
